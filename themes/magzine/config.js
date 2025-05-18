96116caf2c73d28179588b9e68a99b2200e3a3bc
modaiyang <ttsunny@126.com>
2025-05-19 00:57:20 +0800

    Update config.js

--- themes/magzine/config.js
+++ themes/magzine/config.js
@@ -13,10 +13,10 @@ const CONFIG = {
   MAGZINE_HOME_TIPS: '用最接地气的访谈和最前瞻的趋势，为你解码汽车圈',
 
   // 首页底部推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  MAGZINE_RECOMMEND_POST_TAG: '推荐',
-  MAGZINE_RECOMMEND_POST_COUNT: 6,
-  MAGZINE_RECOMMEND_POST_TITLE: '推荐文章',
-  MAGZINE_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  MAGZINE_RECOMMEND_POST_TAG: '',
+  MAGZINE_RECOMMEND_POST_COUNT: 3,
+  MAGZINE_RECOMMEND_POST_TITLE: '最近更新',
+  MAGZINE_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
 
   // Style
   MAGZINE_RIGHT_PANEL_DARK: process.env.NEXT_PUBLIC_MAGZINE_RIGHT_DARK || false, // 右侧面板深色模式
